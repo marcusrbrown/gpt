2b7e34a5affa2c44ff14a7fbdb860e47c55aa67e
Marcus R. Brown <contact@marcusrbrown.com>
2023-12-05 13:28:23 -0700

    chore(prettier): enable `jsxSingleQuote`

--- src/App.tsx
+++ src/App.tsx
@@ -18,36 +18,36 @@ import './App.css';
 function App() {
   return (
     <>
-      <main className="text-foreground bg-background">
+      <main className='text-foreground bg-background'>
         <Navbar>
           <NavbarContent>
             <NavbarItem>
-              <Link isExternal href="https://vitejs.dev">
-                <Image src={viteLogo} className="logo" alt="Vite logo" />
+              <Link isExternal href='https://vitejs.dev'>
+                <Image src={viteLogo} className='logo' alt='Vite logo' />
               </Link>
             </NavbarItem>
             <NavbarItem>
-              <Link isExternal href="https://react.dev">
-                <Image src={reactLogo} className="logo react" alt="React logo" />
+              <Link isExternal href='https://react.dev'>
+                <Image src={reactLogo} className='logo react' alt='React logo' />
               </Link>
             </NavbarItem>
           </NavbarContent>
         </Navbar>
-        <h1 className="text-3xl font-bold py-20">GPT Stuff</h1>
-        <Card className="max-w-[400px]">
-          <CardHeader className="flex gap-3">
+        <h1 className='text-3xl font-bold py-20'>GPT Stuff</h1>
+        <Card className='max-w-[400px]'>
+          <CardHeader className='flex gap-3'>
             <Avatar
-              alt="GPT Logo"
+              alt='GPT Logo'
               isBordered
-              radius="full"
-              size="lg"
-              src="https://files.oaiusercontent.com/file-Oz0yX2PZlcuhJJBeSpJSGkCC?se=2123-10-18T19%3A01%3A39Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D5f8aa2f0-2a0a-4dbd-8c54-861e7625b8fe.png&sig=511Iyj7EEljUU8Zbv2dRFKDCyWNXuQJRQ49EBFBv6d0%3D"
+              radius='full'
+              size='lg'
+              src='https://files.oaiusercontent.com/file-Oz0yX2PZlcuhJJBeSpJSGkCC?se=2123-10-18T19%3A01%3A39Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D5f8aa2f0-2a0a-4dbd-8c54-861e7625b8fe.png&sig=511Iyj7EEljUU8Zbv2dRFKDCyWNXuQJRQ49EBFBv6d0%3D'
             />
-            <div className="flex flex-col gap items-center">
-              <p className="text-md">🏛️ GPT Architect (Advanced Model)</p>
-              <p className="text-small">
+            <div className='flex flex-col gap items-center'>
+              <p className='text-md'>🏛️ GPT Architect (Advanced Model)</p>
+              <p className='text-small'>
                 By{' '}
-                <Link href="https://gpt.mrbro.dev" isExternal showAnchorIcon>
+                <Link href='https://gpt.mrbro.dev' isExternal showAnchorIcon>
                   gpt.mrbro.dev
                 </Link>
               </p>
@@ -59,7 +59,7 @@ function App() {
           </CardBody>
           <Divider />
           <CardFooter>
-            <Link href="https://chat.openai.com/g/g-7uYB9WE9l-gpt-architect-advanced-model" isExternal showAnchorIcon>
+            <Link href='https://chat.openai.com/g/g-7uYB9WE9l-gpt-architect-advanced-model' isExternal showAnchorIcon>
               Chat
             </Link>
           </CardFooter>
