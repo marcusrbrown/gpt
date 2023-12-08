d1970857ab39abb31f028c0db7f45d97bf4d3478
Marcus R. Brown <contact@marcusrbrown.com>
2023-12-08 03:12:15 -0700

    chore: fix linter errors

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,4 @@
-import {FC, useRef, useState} from 'react';
+import {FC, useRef} from 'react';
 import {Avatar, Card as NextUICard, CardBody, CardFooter, CardHeader, Divider, Link} from '@nextui-org/react';
 
 export const Card: FC = () => {
