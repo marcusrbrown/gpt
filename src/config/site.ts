ced3b87ca0c8d09e0d4dfd397c03b6af90750939
Marcus R. Brown <contact@marcusrbrown.com>
2023-12-11 01:35:45 -0700

    feat: add a footer

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,6 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   links: {
+    github: 'https://github.com/marcusrbrown',
     repository: 'https://github.com/marcusrbrown/gpt',
   },
 };
